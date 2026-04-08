92c3d516941e4e58c4ccafbf16ab71b1319cbf6f
jcrzz <juancruzdelvalle12@gmail.com>
2026-04-08 12:49:42 -0300

    feat: add external links to portfolio, update class features, and integrate WhatsApp messaging for bookings

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
-
+  sendWhatsApp(name: string, message: string) {
+    const nameStr = name.trim() ? name : '[Tu nombre]';
+    const msgStr = message.trim() ? message : '[Tu mensaje]';
+    const text = `Hola! mi nombre es ${nameStr}. ${msgStr}`;
+    const url = `https://wa.me/5493446517773?text=${encodeURIComponent(text)}`;
+    window.open(url, '_blank');
+  }
 }
